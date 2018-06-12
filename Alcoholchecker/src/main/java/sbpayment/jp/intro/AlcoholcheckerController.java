73f97a08f6fda092ca6ab2f2c8c20f3d955ef694
kazuki-endo <40160564+kazuki-endo@users.noreply.github.com>
2018-06-12 18:16:02 +0900

    Add files via upload
    ２回目のアップロードです
    パラメータを受け取りコントローラで処理する
    コードを途中まで記述しました。

--- Alcoholchecker/src/main/java/sbpayment/jp/intro/AlcoholcheckerController.java
+++ Alcoholchecker/src/main/java/sbpayment/jp/intro/AlcoholcheckerController.java
@@ -1,12 +1,16 @@
 package sbpayment.jp.intro;
 
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
 
 @Controller
 public class AlcoholcheckerController {
@@ -14,33 +18,73 @@ public class AlcoholcheckerController {
 private JdbcTemplate jdbc;
 
 
+//topページを返す
 @GetMapping("/index")
-public String sample(Model model) {
-//model.addAttribute("name", "hiroki");
-//model.addAttribute("age", 23);
-
-return "index";
+public String index(Model model) {
+	return "index";
 }
 
-@GetMapping("/form")
-public String sample2(String name, int age, Model model) {
+@GetMapping("/check")
+public String check(Model model) {
+	return "check";
 
-model.addAttribute("name", name);
-model.addAttribute("age", age);
-
-return "index";
 }
 
-@PostMapping("/post")
-public String Post(String name, int age,RedirectAttributes attr){
-    attr.addFlashAttribute("name", name);
-    attr.addFlashAttribute("age", age);
+/*@RequestMapping("check")
+ * public String check(){
+ * return "check";
+ * }
+ * 
+
+ @RequestMapping("result")
+ public String result(ModelMap modelmap, @RequestParam("") int a)
+	int sum = a * 450;
+	modelMap.addAttribute("a",a);
+	modelMap.addAttribute("sum",sum);
+	return "result"
+}
+	
+	
+	
+////////入力された値を取得///////////
+/*@RequestMapping(value = "/", method = RequestMethod.POST)
+//public ModelAndView send(
+	@RequestParam(value="check1")int check1,
+	@RequestParam(value="check2")int check2,
+	@RequestParam(value="check3")int check3,
+	@RequestParam(value="check4")int check4,
+	@RequestParam(value="check5")int check5,
+	@RequestParam(value="check6")int check6,
+	ModelAndView mav){
+	
+	入力された値 * DBから取得した値
+	
+//}
+ * 
+ * 
+  ////リダイレクト 
+ * @Requestmapping("/other")
+ * public String other(){
+ * 	return "redirect:"
+ */
+	
+
+
+//結果を表示する
+@GetMapping("/result")
+public String result() {
+	return "result";
+}
+
+//@PostMapping("/post")
+//public String Post(String name, int age,RedirectAttributes attr){
+   // attr.addFlashAttribute("number", number);
     
-    jdbc.update("insert into person (age,name) values(?,?);", age,name);
+    
+    /*jdbc.update("insert into person (age,name) values(?,?);", age,name);
     attr.addFlashAttribute("people",jdbc.queryForList("SELECT * FROM person"));
     //Map<String, Object> person = jdbc.queryForList("SELECT * FROM person where name = ?", name).get(0);
-    return "redirect:/index";
+    return "redirect:/sample";*/
 }
 
 
-}
